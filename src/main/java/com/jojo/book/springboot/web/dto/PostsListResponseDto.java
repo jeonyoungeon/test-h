ec759f90d120ef68faefffd1e60cbcd6288a5438
jeonyoungeon <vndldyd@nate.com>
2020-08-26 17:33:14 +0900

    gitignore 등록

---
+++ src/main/java/com/jojo/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,20 @@
+package com.jojo.book.springboot.web.dto;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jojo.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
